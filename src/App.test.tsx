29ad712d630afe62fd40ffcc11c57c6e27ba01ba
Maysa <maysa@madetech.com>
2019-05-28 16:21:19 +0100

    first test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { default as Adapter } from 'enzyme-adapter-react-16';
+import { configure } from 'enzyme';
+import { shallow } from 'enzyme';
+
+
+configure({ adapter: new Adapter() });
+
+describe('First React component test with Enzyme', () => {
+  it('renders without crashing', () => {
+     shallow(<App />);
+   });
+});
 
 it('renders without crashing', () => {
   const div = document.createElement('div');
