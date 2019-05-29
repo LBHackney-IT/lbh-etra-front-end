ce1a44abacc596a69a3700401b44a5725035ef08
Daniel Williams <daniel@madetech.com>
2019-05-29 15:17:47 +0100

    Undid change

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ const App: React.FC = () => {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p id="react-body">
+        <p>
           Edit <code>src/App.tsx</code> and save to reload.
         </p>
         <a
