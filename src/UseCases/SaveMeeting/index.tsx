c481224cb2371ba35bbf855cb8f3f70e2b8fe537
Pinkham Software Ltd <jeff@pinkhamsoftware.com>
2019-06-05 11:14:52 +0100

    remove example of sample reuqest

--- src/UseCases/SaveMeeting/index.tsx
+++ src/UseCases/SaveMeeting/index.tsx
@@ -33,25 +33,3 @@ export class SaveMeetingUseCase implements ISaveMeetingUseCase{
         return new SaveMeetingOutputModel(true);
     }
 }
-
-
-
-// { 
-//     "estateOfficerId": "1f1bb727-ce1b-e811-8118-70106faa6a31",
-//     "subject": "c1f72d01-28dc-e711-8115-70106faa6a11",
-//     "estateOfficerName": "Megan Holden",
-//     "officerPatchId": "8e958a37-8653-e811-8126-70106faaf8c1",
-//     "areaName": "6",
-//     "managerId": "5512c473-9953-e811-8126-70106faaf8c1",
-//     "serviceRequest": {
-//       "description": "new ETRA meeting issue",
-//       "subject": "c1f72d01-28dc-e711-8115-70106faa6a11",
-//       "createdBy": "1f1bb727-ce1b-e811-8118-70106faa6a31"
-//     },
-//     "traId": "3",
-//     "parentInteractionId":"6d7d967b-7224-e911-a96c-002248072abd",
-//     "issueLocation": "test location 2",
-//     "natureOfEnquiry": "28",
-//     "enquirySubject": "100000219",
-//     "processType":"3"
-//    } 
