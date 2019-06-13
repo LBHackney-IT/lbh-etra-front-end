99eaf0cab7c7cc7dbd83dde54762556126ce1484
Daniel Williams <daniel@madetech.com>
2019-06-13 15:43:26 +0100

    Fixed story so that app can run

--- src/Components/AddIssue/index.stories.tsx
+++ src/Components/AddIssue/index.stories.tsx
@@ -7,6 +7,7 @@ storiesOf('AddIssue',module)
 .add('Opens Correctly',()=>(
     <AddIssue
         index={1} 
+        issue={{Id: "TestId", IssueType: {IssueId: "TestIssueId", IssueType: "TestIssueType"}, Location: { LocationId: "", LocationName: "TestLocationName"}, Notes: ""}}
         onChangeIssue={(issue: IIssue, index: number) => {console.log(`${index}: ${issue.IssueType.IssueType} ${issue.Notes}`)}}
         onDeleteIssue={(index: number) => {console.log(`Delete issue ${index}`)}}/>
 ));
