d34bf9e2a44ee099a8b2c83f2c89ea07fca5613c
Daniel Williams <daniel@madetech.com>
2019-06-13 15:46:33 +0100

    Used factory in storybook

--- src/Components/AddIssue/index.stories.tsx
+++ src/Components/AddIssue/index.stories.tsx
@@ -2,12 +2,15 @@ import React from 'react'
 import AddIssue from '.'
 import {storiesOf} from '@storybook/react'
 import { IIssue } from '../../Domain/Issues';
+import { IssueFactory } from '../../Factories/Issue';
+
+const issueFactory = new IssueFactory();
 
 storiesOf('AddIssue',module)
 .add('Opens Correctly',()=>(
     <AddIssue
         index={1} 
-        issue={{Id: "TestId", IssueType: {IssueId: "TestIssueId", IssueType: "TestIssueType"}, Location: { LocationId: "", LocationName: "TestLocationName"}, Notes: ""}}
+        issue={issueFactory.create()}
         onChangeIssue={(issue: IIssue, index: number) => {console.log(`${index}: ${issue.IssueType.IssueType} ${issue.Notes}`)}}
         onDeleteIssue={(index: number) => {console.log(`Delete issue ${index}`)}}/>
 ));
