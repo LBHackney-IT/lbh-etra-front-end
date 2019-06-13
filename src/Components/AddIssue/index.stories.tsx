7a1848b6fedb8d6459ef6aadc544fdd694d01ab9
Daniel Williams <daniel@madetech.com>
2019-06-13 15:22:26 +0100

    Made some improvements

--- src/Components/AddIssue/index.stories.tsx
+++ src/Components/AddIssue/index.stories.tsx
@@ -1,8 +1,12 @@
 import React from 'react'
 import AddIssue from '.'
 import {storiesOf} from '@storybook/react'
+import { IIssue } from '../../Domain/Issues';
 
 storiesOf('AddIssue',module)
 .add('Opens Correctly',()=>(
-    <AddIssue/>
+    <AddIssue
+        index={1} 
+        onChangeIssue={(issue: IIssue, index: number) => {console.log(`${index}: ${issue.IssueType.IssueType} ${issue.Notes}`)}}
+        onDeleteIssue={(index: number) => {console.log(`Delete issue ${index}`)}}/>
 ));
--- src/Components/AddIssue/index.tsx
+++ src/Components/AddIssue/index.tsx
@@ -1,22 +1,18 @@
-import React from 'react';
+import React, { ChangeEvent } from 'react';
 import { IIssue } from '../../Domain/Issues';
 import './index.css';
 
 export interface IAddIssuesProps
 {
-  onChangeIssues: (issues: IIssue) => void;
+  onChangeIssue: (issue: IIssue, index: number) => void;
   onDeleteIssue: (index:number) => void;
-  index:number;
-  //  issueType:string;
-  //  issueLocation:string;
-  //  notes:string;
-  issue:IIssue;
+  index: number;
+  issue: IIssue;
 }
 
 export interface IAddIssueState
 {
   issue:IIssue;
-  index:number;
 }
 
 export class AddIssue extends React.Component<IAddIssuesProps,IAddIssueState> {
@@ -26,17 +22,14 @@ export class AddIssue extends React.Component<IAddIssuesProps,IAddIssueState> {
       super(props);
 
       this.state={
-        issue:props.issue,
-        index:props.index
+        issue: this.props.issue
       }
     }
-
     deleteIssue(){
-      console.log(`delete issues:${this.props.index}`)
       this.props.onDeleteIssue(this.props.index);
     }
 
-    handleChangeOfIssue=(event:React.ChangeEvent<HTMLInputElement>): void => {
+    handleChangeOfIssue = (event: ChangeEvent<HTMLInputElement>): void => {
       const name = event.target.name;
       const value = event.target.value;
       this.setState({
@@ -46,30 +39,32 @@ export class AddIssue extends React.Component<IAddIssuesProps,IAddIssueState> {
           }
       });
       
-      this.props.onChangeIssues(this.state.issue)
-
+      this.props.onChangeIssue(this.state.issue, this.props.index)
     }
 
-    handleChangeOfIssueNote=(event:React.ChangeEvent<HTMLTextAreaElement>): void => {
-      const name = event.target.name;
+    handleChangeOfIssueNote=(event: ChangeEvent<HTMLTextAreaElement>): void => {
       const value = event.target.value;
-      let issue = this.state.issue;
-      issue.Notes = value;
-      this.setState({issue:issue});
-      this.props.onChangeIssues(this.state.issue)
+      this.setState({
+        issue: { 
+            ...this.state.issue,
+            Notes: value
+        }
+      });
+
+      this.props.onChangeIssue(this.state.issue, this.props.index)
     }
 
     render() {
       return (
         <div>
-          <div>{this.state.index}</div>
+          <div>{this.props.index}</div>
           <div><h4>Record issues at the meeting</h4></div>
           <p>Issue Type</p>
-          <input type="text" id="issue-type"  onChange={ (e) => this.handleChangeOfIssue(e) } />
+          <input type="text" id="issue-type" onChange={this.handleChangeOfIssue} />
           <p>Location</p>
-          <input type="text" id="location"  onChange={ (e) => this.handleChangeOfIssue(e) }/>
+          <input type="text" id="location" name="" onChange={this.handleChangeOfIssue}/>
           <div><p>Issue Notes</p></div>
-          <textarea id="issue-note" value={this.state.issue.Notes} onChange={ (e) => this.handleChangeOfIssueNote(e) }/>
+          <textarea id="issue-note" value={this.state.issue.Notes} onChange={this.handleChangeOfIssueNote }/>
           <button id="delete-issue" className="button btn-primary btn-stacked" data-test="delete-issue" onClick={this.deleteIssue.bind(this)}>Delete Issue</button>
         </div>
       );
