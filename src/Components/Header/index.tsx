252e874ca50d6444d1f28739309afe07a117b7aa
Maysa <maysa@madetech.com>
2019-07-01 11:11:35 +0100

    logo

--- src/Components/Header/index.tsx
+++ src/Components/Header/index.tsx
@@ -1,11 +1,16 @@
 import React, {Component} from 'react';
 import './index.css';
 
+
 export default class Header extends Component {
   public render(){
     return(
       <div className="header">
-        <div className="background-black"><img src="/Users/maysakanoni/Hackney/ManageATeneancy/lbh-etra-front-end/src/HackneyLogo.svg" alt="logo"></img></div>
+        <div className="background-black">
+          <div className="logo-container">
+            <img src={require('../../hackneyLogo.png')} alt="logo" width="206px" height="37px"></img>
+          </div>
+        </div>
         <div className="background-white"></div>
         <div className="background-top-green"></div>
         <div className="divider"></div>
