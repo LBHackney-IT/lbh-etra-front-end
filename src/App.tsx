a1ff0eb796c9817d62254b9482723e46eafe0f3e
Maysa <maysa@madetech.com>
2019-07-01 11:39:01 +0100

    Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import './App.css';
 import AppRouter from './AppRouter';
+import Header from '../src/Components/Header'
 
 const App: React.FC = () => {
   return (
     <div className="App">
+      <Header/>
       <div className="page-wrapper">
           <AppRouter />
       </div>
