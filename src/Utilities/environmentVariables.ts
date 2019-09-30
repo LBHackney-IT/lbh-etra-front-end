e08e5c752510b750c7a0e4add0fcf9994c5a1c89
lbhssandilya <33062415+lbhssandilya@users.noreply.github.com>
2019-09-30 21:12:21 +0100

    changing environment variable for production
    changing environment variable for production

--- src/Utilities/environmentVariables.ts
+++ src/Utilities/environmentVariables.ts
@@ -2,7 +2,7 @@ export default function getEnvVariable(key: string) : string {
     const url = window.location.hostname.toLowerCase();
     let prefix = "REACT_APP_";
     
-    if(url.includes("prod")){
+    if(url.includes("etra.manage-a-tenancy.hackney.gov.uk")){
         prefix += "PROD";
     }
     else if(url.includes("staging")){
