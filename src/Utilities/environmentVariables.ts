33f63b11f86ac6eef28cfbdeb8d68af2fbca6a1d
lbhssandilya <33062415+lbhssandilya@users.noreply.github.com>
2019-10-24 09:39:10 +0100

    Update Prefix in the environment variable according to url prefixes.
    Update Prefix in the environment variable according to url prefixes.

--- src/Utilities/environmentVariables.ts
+++ src/Utilities/environmentVariables.ts
@@ -2,14 +2,14 @@ export default function getEnvVariable(key: string) : string {
     const url = window.location.hostname.toLowerCase();
     let prefix = "REACT_APP_";
     
-    if(url.includes("etra.manage-a-tenancy.hackney.gov.uk")){
-        prefix += "PROD";
+    if(url.includes("dev")){
+        prefix += "DEV";
     }
     else if(url.includes("staging")){
         prefix += "STAGING";
     }
     else {
-        prefix += "DEV";
+        prefix += "PROD";
     }
 
     return process.env[`${prefix}_${key}`] || "";
