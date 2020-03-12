c61603c8c233ce7ab5bb5edcf6972be16a173b22
LBHBomotoso <46450498+LBHBomotoso@users.noreply.github.com>
2020-03-12 14:55:58 +0000

    Focused workflow components
    Workflow separated into single function components

--- src/AppRouter.tsx
+++ src/AppRouter.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter as Router, Route } from "react-router-dom";
 import Meeting from "./Components/Meeting";
 import LandingPage from "./Components/LandingPage";
 import TokenLoader from "./TokenLoader";
+import SaveMeetingETRA from "./Components/SaveMeetingETRA";
 
 export default function AppRouter() {
   return (
@@ -10,6 +11,7 @@ export default function AppRouter() {
       <Route component={TokenLoader} />
       <Route path="/" exact component={LandingPage} />
       <Route path="/meeting/" component={Meeting} />
+      <Route path="/savemeeting/" component={SaveMeetingETRA} />
     </Router>
   );
 }
---
+++ src/Components/SaveMeetingETRA/index.tsx
@@ -0,0 +1,128 @@
+import React from 'react';
+import './index.css';
+import { IIssue } from '../../Domain/Issues'
+import { IServiceProvider, ServiceContext } from '../../ServiceContext';
+import { ISaveMeetingDraftUseCase } from '../../Boundary/SaveMeetingDraft';
+import { MeetingModel, IMeetingModel } from '../../Domain/Meeting';
+import { IAttendees } from '../../Domain/Attendees';
+import { ISignOff } from '../../Domain/SignOff';
+import { Redirect, Link } from 'react-router-dom';
+import Attendees from '../Attendees';
+import RecordIssues from '../RecordIssues';
+export interface ISaveMeetingProps {
+  signOffMode: boolean,
+  traId: number,
+  meetingId?: string,
+  meetingName: string,
+  attendees: IAttendees,
+  issues: Array<IIssue>,
+  signOff: ISignOff,
+  isSessionLive?:boolean
+}
+
+export interface ISaveMeetingState {
+  isAttemptingToSave: boolean;
+  isValid: boolean;
+  redirectToLandingPage: boolean;
+ 
+}
+
+export class SaveMeetingETRA extends React.Component<ISaveMeetingProps, ISaveMeetingState> {
+  public static contextType = ServiceContext;
+  private readonly saveMeetingDraft: ISaveMeetingDraftUseCase;
+  public constructor(props: ISaveMeetingProps, context: IServiceProvider) {
+    super(props, context);
+    this.saveMeetingDraft = context.get<ISaveMeetingDraftUseCase>("ISaveMeetingUseCase");
+    
+    this.state = {
+      isAttemptingToSave: false,
+      isValid: this.checkIsValid(this.props),
+      redirectToLandingPage: false
+    }
+  }
+ 
+  componentWillReceiveProps(newProps: ISaveMeetingProps){
+    this.setState({isValid: this.checkIsValid(newProps)})
+   
+  }
+
+  private checkIsValid(props: ISaveMeetingProps){
+    if(isNaN(props.attendees.attendees) || props.attendees.attendees < 1){
+      return false;
+    }
+    
+    return true;
+  }
+
+  getMeetingModel = () : IMeetingModel => {
+    return new MeetingModel(
+      this.props.traId,
+      this.props.meetingName,
+      this.props.issues, 
+      this.props.attendees, 
+      this.props.signOff,
+      this.props.meetingId, 
+    );
+  }
+
+  handleSaveDraft = () => {
+    this.setState({ isAttemptingToSave: true });
+    const successful = this.saveMeetingDraft.Execute(this.getMeetingModel());
+
+    if (successful) {
+      this.setState({ redirectToLandingPage: true });
+    }
+    else {
+      this.setState({ isAttemptingToSave: false });
+    }
+  }
+
+  renderBackArrow(){
+    return (
+      <>
+        <div className="back-arrow"> &#60;</div>
+        <div className="back-link">
+          <Link to="" id="lnkBack" href="#">Back</Link>
+        </div>
+      </>
+    );
+   
+  };
+
+  render() {
+    if(this.state.redirectToLandingPage){
+      return <Redirect to="" />
+    }
+
+    if(this.state.isAttemptingToSave){
+      return this.renderSpinner();
+    }
+
+    return this.renderSaveMeetingButtons();
+  }
+  
+  private renderSaveMeetingButtons() {
+    return (
+      <div>
+          {this.renderBackArrow()}
+        <button 
+          id="save-draft" 
+          className="button btn-primary btn-stacked button-margin" 
+          onClick={this.handleSaveDraft}
+          disabled={!this.state.isValid}>
+            Save Meeting
+        </button>
+      </div>
+    );
+  }
+
+  private renderSpinner() {
+    return (
+      <div className="spinner-wrapper">
+       <div className="loading-spinner"><div></div><div></div><div></div></div>
+      </div>
+    );
+  }
+}
+
+export default SaveMeetingETRA;
